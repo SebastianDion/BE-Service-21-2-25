4c1fb894f8cb8d03a1b2642d25f91ffd1e778131
Sebastian Dion <v.sebastian.dion@adira.co.id>
2025-02-26 16:56:45 +0700

    ubah jd konsep kafka service

---
+++ nomorTelpReceiver/server.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const mysql = require('mysql2');
+const { Kafka } = require('kafkajs');
+require('dotenv').config();
+
+
+
+const app = express();
+app.use(express.json());
+
+const database = mysql.createConnection({
+  host: process.env.SECOND_DB_HOST,
+  user: process.env.SECOND_DB_USER,
+  password: process.env.SECOND_DB_PASSWORD,
+  database: process.env.SECOND_DB_NAME,
+  port: process.env.SECOND_DB_PORT
+});
+const cors = require("cors");
+app.use(cors()); 
+const kafka = new Kafka({
+  clientId: 'phone-consumer',
+  brokers: ['localhost:9092']
+});
+
+const consumer = kafka.consumer({ groupId: 'phone-group' });
+
+const runConsumer = async () => {
+  await consumer.connect();
+  await consumer.subscribe({topic:'phone-numbers',
+    fromBeginning: true
+  });
+
+  await consumer.run({
+    eachMessage: async ({ message }) =>{
+      const phone = message.value.toString();
+      console.log(`Received phone number: ${phone}`);
+
+      const query = 'INSERT INTO second_user (nomorTelp) VALUES (?)';
+
+      database.query(query, [phone], (err, result) => {
+        if (err) {
+          console.error("Error inserting into second DB:", err);
+        } else {
+          console.log("Phone number saved to second database");
+        }
+      });
+    }
+
+  })
+}
+
+app.get("/", (req, res) => {
+  const sqlQuery = "SELECT * FROM second_user";
+  database.query(sqlQuery, (err, result) => {
+    if (err) {
+      console.error("Error fetching data from second DB:", err);
+      return res.status(500).json({ error: "Database error" });
+    }
+    res.json({ Phone: result });
+  });
+});
+
+
+runConsumer().catch(console.error);
+
+const port = 4000;
+app.listen(port, () => {
+  console.log(`Second backend listening on port ${port}`);
+});
--- projectNumberTelp/index.js
+++ projectNumberTelp/index.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const mysql = require('mysql2');
 const cors = require('cors');  
+const { Kafka } = require('kafkajs');
 require('dotenv').config();
 
 const app = express();
 app.use(cors()); 
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+//Ini mySql Connection
 const database = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
@@ -17,6 +18,22 @@ const database = mysql.createConnection({
   port: process.env.DB_PORT
 });
 
+//Kafka Producer
+const kafka = new Kafka({
+  clientId: 'phone-service',
+  brokers:['localhost:9092']
+});
+const producer = kafka.producer();
+
+const runProducer = async (phone)=>{
+  await producer.connect();
+  await producer.send({
+    topic: 'phone-numbers',
+    messages: [{ value: phone }]
+  });
+  await producer.disconnect();
+}
+
 app.post('/submit', (req, res) => {
   const { phone } = req.body;
   if (!phone) {
@@ -24,11 +41,12 @@ app.post('/submit', (req, res) => {
   }
 
   const query = 'INSERT INTO user (nomorTelp) VALUES (?)';
-  database.query(query, [phone], (err, result) => {
+  database.query(query, [phone], async (err, result) => {
       if (err) {
           console.error(err);
           return res.status(500).json({ error: "Database error" });
       }
+      await runProducer(phone);
       res.json({ message: "Phone number inserted successfully" });
   });
 });
